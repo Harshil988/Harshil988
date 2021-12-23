a31ca7db6c814e3dfc50b776c9ccf6c1fff43f79
Harshit <harshilsakhiya9886@gmail.com>
2021-12-23 10:40:49 +0530

    final no edit and done

--- src/List.jsx
+++ src/List.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useDispatch } from "react-redux"
 import  {newcontect} from "./Action/Action"
 import shortid from 'shortid';
-// import {useHisroty} from 'react-router-dom'
 
 
 export default function List() {
